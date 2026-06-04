89970fb0232b35ce938b04c43366ba25206967db
Ravikiran <171322216+ravikiranoffl@users.noreply.github.com>
2026-06-04 17:44:34 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open('vani-v1').then((cache) => cache.addAll(['./index.html', './style.css', './script.js'])));
+});
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
