83e8a83a634232ace54803ee8636c067b2ca8871
ravikiranoffl <sololearnh@gmail.com>
2026-06-18 06:38:04 +0530

    sw.js and manifest.json added

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "vani-matrix-cache-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("📦 VANI Service Worker: Caching core matrix files");
+      return cache.addAll(ASSETS_TO_CACHE);
+    }),
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    }),
+  );
+});
